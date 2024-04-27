8533db8cd2bc351021d7fb8e0f7d9c8821d70a80
MarcienD <114868544+MarcienD@users.noreply.github.com>
2024-04-27 16:02:39 +0200

    Kod zlintowany
    Poprawiona indentacja i przeniesiony import.

--- wycena_dxf/app.py
+++ wycena_dxf/app.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import os
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
-import os
 from wycena_dxf import analyze_dxf
 
 app = Flask(__name__)
@@ -16,26 +16,26 @@ def index():
 @app.route('/analyze', methods=['POST'])
 def analyze():
  
-        material = request.form['material']
-        thickness = request.form['thickness']
-        require_material = bool(request.form['requireMaterial'])
-        file = request.files['file']
+    material = request.form['material']
+    thickness = request.form['thickness']
+    require_material = bool(request.form['requireMaterial'])
+    file = request.files['file']
 
-        # Zapisanie przeslanego pliku DXF
-        file_path = 'temp.dxf'
-        file.save(file_path)
+    # Zapisanie przeslanego pliku DXF
+    file_path = 'temp.dxf'
+    file.save(file_path)
 
-        # Wywolanie funkcji z Twojej aplikacji do analizy pliku DXF
-        total_perimeter, total_area = analyze_dxf(file_path)
+    # Wywolanie funkcji z Twojej aplikacji do analizy pliku DXF
+    total_perimeter, total_area = analyze_dxf(file_path)
 
-        # Usuniecie tymczasowego pliku DXF
-        os.remove(file_path)
+    # Usuniecie tymczasowego pliku DXF
+    os.remove(file_path)
 
-        # Zwrocenie wynikow jako odpowied� JSON
-        return jsonify({
-            'total_perimeter': round(total_perimeter, 1),
-            'total_area': round(total_area, 1)
-        })
+    # Zwrocenie wynikow jako odpowied� JSON
+    return jsonify({
+        'total_perimeter': round(total_perimeter, 1),
+        'total_area': round(total_area, 1)
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
